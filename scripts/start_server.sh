03134257c77272c85c61463f31b62ff0fca4dfe0
sai kiran <104380617+saikiranbanka749@users.noreply.github.com>
2026-03-11 19:41:57 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+echo "Starting Flask Application"
+
 cd /home/ubuntu/python-app
 
-# Install dependencies just in case
-pip3 install -r requirements.txt
+# Install required system packages
+sudo apt update
+sudo apt install -y python3-pip python3-venv
 
-# Start Flask app in background
+# Create virtual environment if not exists
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
+# Activate venv
+source venv/bin/activate
+
+# Install python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Start Flask app
 nohup python3 app.py > app.log 2>&1 &
