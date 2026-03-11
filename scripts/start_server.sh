468111b5ed479eacae215f732fecd5545eb95441
sai kiran <104380617+saikiranbanka749@users.noreply.github.com>
2026-03-11 19:35:45 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Starting Flask Application"
-
 cd /home/ubuntu/python-app
 
+# Install dependencies just in case
 pip3 install -r requirements.txt
 
+# Start Flask app in background
 nohup python3 app.py > app.log 2>&1 &
